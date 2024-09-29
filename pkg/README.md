a2cf0a40f07850720877d7006043f52cf665e444
Lewis Belcher <belcher_lewis@protonmail.com>
2024-09-29 11:12:46 +0200

    Add wasm files

---
+++ pkg/README.md
@@ -0,0 +1,18 @@
+# WebAssembly QR Code Decoder
+
+A small server for displaying low level information about QR codes by decoding
+them on client-side using WebAssembly.
+
+Test out the deployed example at https://qr.deftonthe.tech
+
+<details>
+
+<summary>TODO</summary>
+
+- Add better styling
+- Try using a canvas to draw the image and retrieve image data from there
+- Draw QR code crops
+- Use `wee_alloc`
+- Try removing `std`
+
+</details>
---
+++ pkg/wasm_qr.d.ts
@@ -0,0 +1,51 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function main(): void;
+/**
+* @param {number} width
+* @param {number} height
+* @param {Uint8Array} data
+* @returns {Uint8Array}
+*/
+export function decode_qr(width: number, height: number, data: Uint8Array): Uint8Array;
+/**
+* @param {FileReader} file_reader
+*/
+export function log_image_qr_data(file_reader: FileReader): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly main: () => void;
+  readonly decode_qr: (a: number, b: number, c: number) => number;
+  readonly log_image_qr_data: (a: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
